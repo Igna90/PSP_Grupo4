64a43b6001edc4a3725b76ad4833eb7c04082720
AlbitaMari <albasaenz89@gmail.com>
2022-02-03 18:08:10 +0100

    ok

--- nucleo/views.py
+++ nucleo/views.py
@@ -301,7 +301,7 @@ class ActiveUserView(UpdateView):
                 return render(self.request,"nucleo/admin/user_list.html",{'users':users})
         
 @method_decorator(is_not_admin,name="dispatch")
-# @method_decorator(is_active,name="dispatch")
+@method_decorator(is_active,name="dispatch")
 class ProjectListView(ListView):
     model=Project
     template_name="nucleo/users/project_list.html"
